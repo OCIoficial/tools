04d2fd0501c228d9a70b65444e729b7714593164
Nico Lehmann <nico.lehmannm@gmail.com>
2021-05-06 16:16:55 -0700

    fix

--- create_participants.sh
+++ create_participants.sh
@@ -33,14 +33,14 @@ read DB_PASS
 stty $stty_orig
 export PGPASSWORD=$DB_PASS
 
-psql -h localhost -U cmsuser -d cmsdb -c "START TRANSACTION;"
 COUNT=0
-while IFS=, read -r NAME LASTNAME USER PASS MAIL
+while IFS=, read -r NAME LASTNAME USER PASS MAIL PL
 do
     USER_ID=`psql -h localhost -U cmsuser -d cmsdb -c "INSERT INTO users(first_name,last_name,username,password,email,preferred_languages) VALUES('${NAME}','${LASTNAME}','${USER}','plaintext:${PASS}','${MAIL}','{}') returning id;" | awk 'FNR == 3 {print}'`
     psql -h localhost -U cmsuser -d cmsdb -c "INSERT INTO participations(delay_time,extra_time,hidden,unrestricted,contest_id,user_id) VALUES('00:00:00','00:00:00',false,false,${CONTEST_ID},${USER_ID});"
+
+    COUNT=$((COUNT+1))
 done < "$FILENAME"
-psql -h localhost -U cmsuser -d cmsdb -c "COMMIT;"
 
 echo "Succesfully registered ${COUNT} participants in contest ${CONTEST_ID}"
 unset PGPASSWORD
